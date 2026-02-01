d496d9ece54a18fba5010f9f5a73b84a8f1732d3
Vgentry10 <gen23003@byui.edu>
2026-01-31 17:27:29 -0700

    responsive.js and ponder/modal.js to add interactivity to responsive navigation and modal gallery.

---
+++ ponder/responsive.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    nav.classList.toggle('hide');
+    btn.classList.toggle('change');
+    
+}
+  
